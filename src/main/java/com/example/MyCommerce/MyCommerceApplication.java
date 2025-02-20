12355f1068fed8b6b54dcd965ee1d585d303d25b
steven wang <steven.wang@pactera.com>
2025-02-20 16:07:49 +0800

    first

---
+++ src/main/java/com/example/MyCommerce/MyCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.MyCommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCommerceApplication.class, args);
+	}
+
+}
